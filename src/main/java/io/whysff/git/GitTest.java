d320e580d0f727bf57c8b0f5a0440f10efbc0d95
whysff <15670151791@126.com>
2022-06-03 23:50:04 +0800

    first commit

---
+++ src/main/java/io/whysff/git/GitTest.java
@@ -0,0 +1,12 @@
+package io.whysff.git;
+
+/**
+ * @author lxstart  Email:liuxuan1021@126.com
+ * @create 2022/06/03
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,git!");
+    }
+}
